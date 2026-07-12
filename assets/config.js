ac71e040d1fa4e68e1235a3bb64488938d6ed398
cnduy1192 <duy.chengoc@fisaigon.vn>
2026-07-12 13:31:01 +0700

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
-  GOOGLE_SHEET_ENDPOINT: "https://script.google.com/macros/s/AKfycbzuOcdxRvrnGMZoBE0WkZJtm3farNCtHQ4_Q5kCxAnyeF9HYg735fk6pcYC9nxce71A/exec",
+  GOOGLE_SHEET_ENDPOINT: "https://script.google.com/macros/s/AKfycbzEkLnxLW-hdHyNJf6o3Bh9qzKU9CrXrCUm4e6zvvL3aU4mOMcgKdFEzAqMbI4Xo0_Gdw/exec",
   SUPER_ADMIN_ID: "kba-admin",
-  SUPER_ADMIN_PASSWORD: "KBA@2026",
-  SUPER_ADMIN_GMAIL: "admin@kbacarrental.vn",
+  SUPER_ADMIN_PASSWORD: "1lonCoc@",
+  SUPER_ADMIN_GMAIL: "cheduy1192@gmail.com",
 
   // Hotline hiển thị nổi góc dưới trái
   HOTLINE: "0936848404",
@@ -12,6 +12,6 @@ window.APP_CONFIG = {
 
   // Link mạng xã hội (icon nổi phía dưới trang)
   SOCIAL_ZALO: "https://zalo.me/0936848404",
-  SOCIAL_FACEBOOK: "https://facebook.com/",
-  SOCIAL_TIKTOK: "https://tiktok.com/"
+  SOCIAL_FACEBOOK: "https://www.facebook.com/MYN.Coffee",
+  SOCIAL_TIKTOK: "https://www.tiktok.com/@thangtaixe94?_r=1"
 };
